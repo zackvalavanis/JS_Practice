77fd684322c006c7dc67ed24097efb7683f79927
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-14 16:49:45 -0500

    js practice

--- js7.js
+++ js7.js
@@ -1,6 +1,6 @@
 // Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
-array = [];
+var array = [];
 
 array.push('hello','goodbye', 'hi');
 
---
+++ js8.js
@@ -0,0 +1,13 @@
+// Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+var array = [1, 2];
+var i = 0;
+
+
+while(i < array.length) { 
+  var a = array[i];
+  console.log(a);
+  i += 1;
+}
+
+
