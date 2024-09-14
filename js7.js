1f6a5a7cec3e8a42845455fe68780f3e8898eace
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-14 16:47:34 -0500

    js practice

---
+++ js7.js
@@ -0,0 +1,8 @@
+// Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+array = [];
+
+array.push('hello','goodbye', 'hi');
+
+console.log(array);
+
